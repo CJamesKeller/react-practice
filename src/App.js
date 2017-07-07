b251f95a59a34fcb60184a2bb72e52e648b74f5f
Chris Keller <cjameskeller@gmail.com>
2017-07-06 20:32:23 -0500

    input not working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
 import React from 'react';
 import './App.css';
 
-class Button extends React.Component {
+class App extends React.Component {
+  state = {
+    counter: 0,
+    input: "",
+    message: ""
+  };
+  increment = (incrementAmount) => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + incrementAmount
+    }));
+  };
+  decrement = (decrementAmount) => {
+    this.setState((prevState) => ({
+      counter: prevState.counter - decrementAmount
+    }));
+  };
+  write = (input) => {
+    console.log("input", this.state.input);
+    console.log("message", this.state.message);
+    this.setState(() => ({
+      input: input,
+      message: input
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <div className="mainDiv">
+          <div className="Header">
+            <Header />
+          </div>
+          <div className="Content">
+            <ButtonUp incrementAmount={1} onClickFunction={this.increment} />
+            <ButtonUp incrementAmount={5} onClickFunction={this.increment} />
+            <ButtonDown decrementAmount={1} onClickFunction={this.decrement} />
+            <ButtonDown decrementAmount={5} onClickFunction={this.decrement} />
+            <Result counter={this.state.counter} />
+          </div>
+          <div className="notes">
+            <NoteInput onChangeFunction={this.write} />
+            <div className="messages">
+              <Message />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+}
+
+const Header = () => {
+  return (
+    <div>
+      <h1>You may add or subtract</h1>
+    </div>
+  );
+}
+
+class ButtonUp extends React.Component {
   handleClick = () => {
     this.props.onClickFunction(this.props.incrementAmount);
   };
@@ -11,32 +69,45 @@ class Button extends React.Component {
         +{this.props.incrementAmount}
       </button>
     );
-  }
+  };
+}
+
+class ButtonDown extends React.Component {
+  handleClick = () => {
+    this.props.onClickFunction(this.props.decrementAmount);
+  };
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        -{this.props.decrementAmount}
+      </button>
+    );
+  };
 }
 
 const Result = (props) => {
   return (
     <div>{props.counter}</div>
   );
-};
+}
 
-class App extends React.Component {
-  state = { counter: 0 };
-  increment = (incrementAmount) => {
-    this.setState((prevState) => ({
-      counter: prevState.counter + incrementAmount
-    }));
+class NoteInput extends React.Component {
+  handleChange = () => {
+    this.props.onChangeFunction(this.props.input);
+    console.log("input2", this.props.input);
+    console.log("message2", this.props.message);
   };
   render() {
     return (
-      <div>
-        <div className="mainDiv">
-          <Button incrementAmount={1} onClickFunction={this.increment} />
-          <Button incrementAmount={5} onClickFunction={this.increment} />
-          <Result counter={this.state.counter} />
-        </div>
-      </div>
+      <input type="text" value={this.props.input} onChange={this.handleChange}></input>
     );
   };
 }
+
+const Message = (props) => {
+  return (
+    <div>{props.message}</div>
+  );
+}
+
 export default App;
