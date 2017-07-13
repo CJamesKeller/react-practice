02975794a47d1710b6680ceb8e0a204c540e05cd
Chris Keller <cjameskeller@gmail.com>
2017-07-13 13:37:09 -0500

    data binding text input

--- src/App.js
+++ src/App.js
@@ -3,28 +3,15 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    counter: 0,
-    input: "",
-    message: ""
+    written: "",
   };
-  increment = (incrementAmount) => {
-    this.setState((prevState) => ({
-      counter: prevState.counter + incrementAmount
-    }));
-  };
-  decrement = (decrementAmount) => {
-    this.setState((prevState) => ({
-      counter: prevState.counter - decrementAmount
-    }));
-  };
-  write = (input) => {
-    console.log("input", this.state.input);
-    console.log("message", this.state.message);
+
+  write = (written) => {
     this.setState(() => ({
-      input: input,
-      message: input
+      written: written
     }));
   };
+
   render() {
     return (
       <div>
@@ -33,17 +20,8 @@ class App extends React.Component {
             <Header />
           </div>
           <div className="Content">
-            <ButtonUp incrementAmount={1} onClickFunction={this.increment} />
-            <ButtonUp incrementAmount={5} onClickFunction={this.increment} />
-            <ButtonDown decrementAmount={1} onClickFunction={this.decrement} />
-            <ButtonDown decrementAmount={5} onClickFunction={this.decrement} />
-            <Result counter={this.state.counter} />
-          </div>
-          <div className="notes">
-            <NoteInput onChangeFunction={this.write} />
-            <div className="messages">
-              <Message />
-            </div>
+            <WritingForm write={this.write}/>
+            <Result written={this.state.written}/>
           </div>
         </div>
       </div>
@@ -54,59 +32,27 @@ class App extends React.Component {
 const Header = () => {
   return (
     <div>
-      <h1>You may add or subtract</h1>
+      <h1>Write your message!</h1>
     </div>
   );
 }
 
-class ButtonUp extends React.Component {
-  handleClick = () => {
-    this.props.onClickFunction(this.props.incrementAmount);
-  };
-  render() {
+const WritingForm = ({ write }) => {
+    let input;
     return (
-      <button onClick={this.handleClick}>
-        +{this.props.incrementAmount}
-      </button>
+        <div>
+            <input type="text" ref={node => input = node} />
+            <button onClick={() => write(input.value)}>Write!</button>
+        </div>
     );
-  };
 }
 
-class ButtonDown extends React.Component {
-  handleClick = () => {
-    this.props.onClickFunction(this.props.decrementAmount);
-  };
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        -{this.props.decrementAmount}
-      </button>
-    );
-  };
-}
-
-const Result = (props) => {
+const Result = ({written}) => {
   return (
-    <div>{props.counter}</div>
-  );
-}
-
-class NoteInput extends React.Component {
-  handleChange = () => {
-    this.props.onChangeFunction(this.props.input);
-    console.log("input2", this.props.input);
-    console.log("message2", this.props.message);
-  };
-  render() {
-    return (
-      <input type="text" value={this.props.input} onChange={this.handleChange}></input>
-    );
-  };
-}
-
-const Message = (props) => {
-  return (
-    <div>{props.message}</div>
+    <div>
+      <h2>Message:</h2>
+      <p>{written}</p>
+    </div>
   );
 }
 
